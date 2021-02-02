beeb84a0b71f58af279b6798fd8745abfd009665
Aleksey Titov <talexden@gmail.com>
2021-02-02 22:56:07 +0300

    Пересобрал страницу, склеил комиты
    Главная страница, сборка
    
    Шаблонизация
    
    Добавочки
    
    повторный пуш
    
    Обновления под актуальный код

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,32 @@
-const {src, watch, series, parallel} = require('gulp');
+const {src, dest, watch, series, parallel} = require('gulp');
 const htmlValidator = require('gulp-w3c-html-validator');
 const htmlhint = require('gulp-htmlhint');
 const lintspaces = require('gulp-lintspaces');
 const stylelint = require('gulp-stylelint');
+const twig = require('gulp-twig');
+const data = require('gulp-data');
+const svgstore = require('gulp-svgstore');
+const htmlBeautify = require('gulp-html-beautify');
+const less = require('gulp-less');
+const postcss = require('gulp-postcss');
+const imagemin = require('gulp-imagemin');
+const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
+
+const SVGO_PLUGINS_CONFIG = {
+	floatPrecision: 2
+};
+const SVGO_CONFIG = {
+	plugins: [
+		{ removeViewBox: false },
+		{ removeTitle: true },
+		{ cleanupNumericValues: SVGO_PLUGINS_CONFIG },
+		{ convertPathData: SVGO_PLUGINS_CONFIG },
+		{ transformsWithOnePath: SVGO_PLUGINS_CONFIG },
+		{ convertTransform: SVGO_PLUGINS_CONFIG },
+		{ cleanupListOfValues: SVGO_PLUGINS_CONFIG }
+	]
+};
 
 const htmlTest = () => src('*.html')
 	.pipe(lintspaces({
@@ -15,12 +38,44 @@ const htmlTest = () => src('*.html')
 	.pipe(htmlValidator())
 	.pipe(htmlValidator.reporter());
 
-const cssTest = () => src('css/**/*.css')
+const htmlBuild = () => src('src/twig/pages/**/*.twig')
+	.pipe(data((file) => {
+		return {
+			page: file.path.replace(/\\/g, '/').replace(/^.*?twig\/pages\/(.*)\.twig$/, '$1')
+		};
+	}))
+	.pipe(twig())
+	.pipe(htmlBeautify())
+	.pipe(dest('.'));
+
+const cssTest = () => src('src/less/**/*.less')
 	.pipe(lintspaces({
     editorconfig: '.editorconfig'
   }))
   .pipe(lintspaces.reporter())
-	.pipe(stylelint());
+	.pipe(stylelint({
+    reporters: [
+      {
+        console: true,
+        formatter: 'string'
+      }
+    ]
+  }));
+
+const cssBuild = () => src('src/less/style.less')
+  .pipe(less())
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+  .pipe(dest(`css`));
+
+// Сборка спрайта
+const spriteBuild = () => src('src/sprite/**/*.svg')
+  .pipe(imagemin([imagemin.svgo(SVGO_CONFIG)]))
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(dest(`img`));
 
 const reload = (done) => {
   browserSync.reload();
@@ -35,11 +90,14 @@ const watchTask = () => {
     ui: false
   });
 
-	watch('**/*.html', series(htmlTest, reload));
-	watch('css/**/*.css', series(cssTest, reload));
+	watch('src/twig/**/*.twig', series(htmlBuild, htmlTest, reload));
+	watch('src/less/**/*.less', series(cssTest, cssBuild, reload));
+	watch('src/sprite/**/*.svg', series(cssTest, spriteBuild, reload));
 };
 
 const test = parallel(htmlTest, cssTest);
+const build = parallel(htmlBuild, cssBuild, spriteBuild);
 
 exports.test = test;
-exports.default = series(test, watchTask);
+exports.build = build;
+exports.default = series(test, build, watchTask);
