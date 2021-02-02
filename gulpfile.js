f73b9a6905d670e85098379b9b58d35ca9ba38ca
Aleksey Titov <talexden@gmail.com>
2021-02-02 22:56:07 +0300

    Главная страница, сборка

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
-const {src, watch, series, parallel} = require('gulp');
+const {src, dest, watch, series, parallel} = require('gulp');
 const htmlValidator = require('gulp-w3c-html-validator');
 const htmlhint = require('gulp-htmlhint');
 const lintspaces = require('gulp-lintspaces');
 const stylelint = require('gulp-stylelint');
+const svgstore = require('gulp-svgstore');
+const less = require('gulp-less');
+const postcss = require('gulp-postcss');
+const imagemin = require('gulp-imagemin');
+const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
+
+const SVGO_PLUGINS_CONFIG = {
+	floatPrecision: 2
+};
+const SVGO_CONFIG = {
+	plugins: [
+		{ removeViewBox: false },
+		{ removeTitle: true },
+		{ cleanupNumericValues: SVGO_PLUGINS_CONFIG },
+		{ convertPathData: SVGO_PLUGINS_CONFIG },
+		{ transformsWithOnePath: SVGO_PLUGINS_CONFIG },
+		{ convertTransform: SVGO_PLUGINS_CONFIG },
+		{ cleanupListOfValues: SVGO_PLUGINS_CONFIG }
+	]
+};
 
 const htmlTest = () => src('*.html')
 	.pipe(lintspaces({
@@ -15,12 +35,34 @@ const htmlTest = () => src('*.html')
 	.pipe(htmlValidator())
 	.pipe(htmlValidator.reporter());
 
-const cssTest = () => src('css/**/*.css')
+const cssTest = () => src('src/less/**/*.less')
 	.pipe(lintspaces({
     editorconfig: '.editorconfig'
   }))
   .pipe(lintspaces.reporter())
-	.pipe(stylelint());
+	.pipe(stylelint({
+    reporters: [
+      {
+        console: true,
+        formatter: 'string'
+      }
+    ]
+  }));
+
+const cssBuild = () => src('src/less/style.less')
+  .pipe(less())
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+  .pipe(dest(`css`));
+
+// Сборка спрайта
+const spriteBuild = () => src('src/sprite/**/*.svg')
+  .pipe(imagemin([imagemin.svgo(SVGO_CONFIG)]))
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(dest(`img`));
 
 const reload = (done) => {
   browserSync.reload();
@@ -36,10 +78,13 @@ const watchTask = () => {
   });
 
 	watch('**/*.html', series(htmlTest, reload));
-	watch('css/**/*.css', series(cssTest, reload));
+	watch('src/less/**/*.less', series(cssTest, cssBuild, reload));
+	watch('src/sprite/**/*.svg', series(cssTest, spriteBuild, reload));
 };
 
 const test = parallel(htmlTest, cssTest);
+const build = parallel(cssBuild, spriteBuild);
 
 exports.test = test;
-exports.default = series(test, watchTask);
+exports.build = build;
+exports.default = series(test, build, watchTask);
