8833b5f36808108e8e587fefe0b4f1a8b8370ed3
Aleksey Titov <talexden@gmail.com>
2021-03-21 20:06:08 +0300

    iframe - карта, анимация появления - исчезание

--- js/sedona.js
+++ js/sedona.js
@@ -2,14 +2,7 @@ const searchButton = document.querySelector('.search-button');
 const searchForm = document.querySelector('.search-form');
 const searchFields = searchForm.querySelectorAll('[name]');
 
-
 let isStorageSupport = true;
-
-// let memDateOfCheckIn;
-// let memDateOfCheckOut;
-// let memAdults;
-// let memChildren
-// let tryStorage;
 
 try {
   localStorage.getItem('test');
@@ -26,67 +19,8 @@ if (isStorageSupport) {
   }
 }
 
-
-
 searchButton.addEventListener('click', function () {
   searchForm.classList.toggle('search-form-show');
   searchForm.classList.toggle('search-form-hide');
-  // searchForm.classList.remove('search-form-error');
 });
 
-// if (memDateOfCheckIn) {
-//   dateOfCheckIn.value = memDateOfCheckIn
-// }
-// if (memDateOfCheckOut) {
-//   dateOfCheckOut.value = memDateOfCheckOut
-// }
-// if (memAdults) {
-//   adults.value = memAdults
-// }
-// if (memChildren) {
-//   children.value = memChildren
-// }
-
-// searchForm.addEventListener('submit', function (evt) {
-//   if (searchForm.classList.contains('search-form-error')) {
-//     searchForm.classList.remove('search-form-error');
-//   }
-//   if (!dateOfCheckIn.value) {
-//     evt.preventDefault();
-//     dateOfCheckIn.focus();
-//     searchForm.classList.add('search-form-error');
-//   } else {
-//     if (isStorageSupport) {
-//       localStorage.setItem('memDateOfCheckIn', dateOfCheckIn.value);
-//     }
-//     if (!dateOfCheckOut.value) {
-//       evt.preventDefault();
-//       dateOfCheckOut.focus();
-//     } else {
-//       if (isStorageSupport) {
-//         localStorage.setItem('dateOfCheckOut', memDateOfCheckOut.value);
-//       }
-//       if (!adults.value) {
-//         evt.preventDefault();
-//         adults.focus();
-//       } else {
-//         if (isStorageSupport) {
-//           localStorage.setItem('memAdults', adults.value);
-//           localStorage.setItem('memChildren', children.value);
-//         }
-//       }
-//     }
-//   }
-// })
-
-// window.addEventListener('keydown', function (evt) {
-//   if (evt.keyCode === 27) {
-//     if (searchForm.classList.contains('search-form-show')) {
-//       searchForm.classList.remove('search-form-show');
-//     }
-//   }
-
-//   if (searchForm.classList.contains('search-form-error')) {
-//     searchForm.classList.remove('search-form-error');
-//   }
-// })
