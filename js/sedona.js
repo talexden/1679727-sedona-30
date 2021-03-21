f1bbc6b9c30743797db2f163002b3cab9bbf80d7
Aleksey Titov <talexden@gmail.com>
2021-03-21 16:03:25 +0300

    js и Анимация

--- js/sedona.js
+++ js/sedona.js
@@ -1,83 +1,92 @@
 const searchButton = document.querySelector('.search-button');
 const searchForm = document.querySelector('.search-form');
-const dateOfCheckIn = searchForm.querySelector('[name=date-of-check-in]');
-const dateOfCheckOut = searchForm.querySelector('[name=date-of-check-out]');
-const adults = searchForm.querySelector('[name=adults]');
-const children = searchForm.querySelector('[name=children]');
+const searchFields = searchForm.querySelectorAll('[name]');
 
-let memDateOfCheckIn;
-let memDateOfCheckOut;
-let memAdults;
-let memChildren
+
 let isStorageSupport = true;
-let tryStorage;
+
+// let memDateOfCheckIn;
+// let memDateOfCheckOut;
+// let memAdults;
+// let memChildren
+// let tryStorage;
 
 try {
-  tryStorage = localStorage.getItem('memDateOfCheckIn')
+  localStorage.getItem('test');
 } catch (err) {
   isStorageSupport = false;
 }
 
+if (isStorageSupport) {
+  for (const field of searchFields) {
+    const storedValue = localStorage.getItem(field.name);
+    if (storedValue) {
+      field.value = storedValue;
+    }
+  }
+}
+
+
+
 searchButton.addEventListener('click', function () {
   searchForm.classList.toggle('search-form-show');
-  if (searchForm.classList.contains('search-form-error')) {
-    searchForm.classList.remove('search-form-error');
-  }
+  searchForm.classList.toggle('search-form-hide');
+  // searchForm.classList.remove('search-form-error');
 });
 
-if (memDateOfCheckIn) {
-  dateOfCheckIn.value = memDateOfCheckIn
-}
-if (memDateOfCheckOut) {
-  dateOfCheckOut.value = memDateOfCheckOut
-}
-if (memAdults) {
-  adults.value = memAdults
-}
-if (memChildren) {
-  children.value = memChildren
-}
+// if (memDateOfCheckIn) {
+//   dateOfCheckIn.value = memDateOfCheckIn
+// }
+// if (memDateOfCheckOut) {
+//   dateOfCheckOut.value = memDateOfCheckOut
+// }
+// if (memAdults) {
+//   adults.value = memAdults
+// }
+// if (memChildren) {
+//   children.value = memChildren
+// }
 
-searchForm.addEventListener('submit', function (evt) {
-  if (searchForm.classList.contains('search-form-error')) {
-    searchForm.classList.remove('search-form-error');
-  }
-  if (!dateOfCheckIn.value) {
-    evt.preventDefault();
-    dateOfCheckIn.focus();
-    searchForm.classList.add('search-form-error');
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem('memDateOfCheckIn', dateOfCheckIn.value);
-    }
-    if (!dateOfCheckOut.value) {
-      evt.preventDefault();
-      dateOfCheckOut.focus();
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem('dateOfCheckOut', memDateOfCheckOut.value);
-      }
-      if (!adults.value) {
-        evt.preventDefault();
-        adults.focus();
-      } else {
-        if (isStorageSupport) {
-          localStorage.setItem('memAdults', adults.value);
-          localStorage.setItem('memChildren', children.value);
-        }
-      }
-    }
-  }
-})
+// searchForm.addEventListener('submit', function (evt) {
+//   if (searchForm.classList.contains('search-form-error')) {
+//     searchForm.classList.remove('search-form-error');
+//   }
+//   if (!dateOfCheckIn.value) {
+//     evt.preventDefault();
+//     dateOfCheckIn.focus();
+//     searchForm.classList.add('search-form-error');
+//   } else {
+//     if (isStorageSupport) {
+//       localStorage.setItem('memDateOfCheckIn', dateOfCheckIn.value);
+//     }
+//     if (!dateOfCheckOut.value) {
+//       evt.preventDefault();
+//       dateOfCheckOut.focus();
+//     } else {
+//       if (isStorageSupport) {
+//         localStorage.setItem('dateOfCheckOut', memDateOfCheckOut.value);
+//       }
+//       if (!adults.value) {
+//         evt.preventDefault();
+//         adults.focus();
+//       } else {
+//         if (isStorageSupport) {
+//           localStorage.setItem('memAdults', adults.value);
+//           localStorage.setItem('memChildren', children.value);
+//         }
+//       }
+//     }
+//   }
+// })
 
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27) {
-    if (searchForm.classList.contains('search-form-show')) {
-      searchForm.classList.remove('search-form-show');
-    }
-  }
+// window.addEventListener('keydown', function (evt) {
+//   if (evt.keyCode === 27) {
+//     if (searchForm.classList.contains('search-form-show')) {
+//       searchForm.classList.remove('search-form-show');
+//     }
+//   }
 
-  if (searchForm.classList.contains('search-form-error')) {
-    searchForm.classList.remove('search-form-error');
-  }
-})
+//   if (searchForm.classList.contains('search-form-error')) {
+//     searchForm.classList.remove('search-form-error');
+//   }
+// })
